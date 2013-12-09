288ec6836483d2bdfae1ce1c36feb797cc658a0a
Danijel Kavčič <danijel.kavcic@gmail.com>
2013-12-10 00:42:07 +0100

    misc

--- client/helpers.js
+++ client/helpers.js
@@ -1,7 +1,44 @@
-var DateFormats = {
+DateFormats = {
  short: "DD.MM.YYYY",
  long: "dddd DD.MM.YYYY HH:mm"
 };
+
+////////// Helpers for in-place editing //////////
+
+// Returns an event map that handles the "escape" and "return" keys and
+// "blur" events on a text input (given by selector) and interprets them
+// as "ok" or "cancel".
+okCancelEvents = function (selector, callbacks) {
+  var ok = callbacks.ok || function () {};
+  var cancel = callbacks.cancel || function () {};
+
+  var events = {};
+  events['keyup '+selector+', keydown '+selector+', focusout '+selector] =
+    function (evt) {
+      if (evt.type === "keydown" && evt.which === 27) {
+        // escape = cancel
+        cancel.call(this, evt);
+
+      } else if (evt.type === "keyup" && evt.which === 13 ||
+                 evt.type === "focusout") {
+        // blur/return/enter = ok/submit if non-empty
+        var value = String(evt.target.value || "");
+        if (value)
+          ok.call(this, value, evt);
+        else
+          cancel.call(this, evt);
+      }
+    };
+
+  return events;
+};
+
+activateInput = function (input) {
+  input.focus();
+  input.select();
+};
+
+////////// Handlebars helpers /////////////
 
 Handlebars.registerHelper("formatDate", function(datetime, format) {
   if (moment) {
@@ -14,45 +51,9 @@ Handlebars.registerHelper("formatDate", function(datetime, format) {
 });
 
 Handlebars.registerHelper("stars", function(rate) {
-	var rateString = "";
-	for (var i = 0; i < rate; i++) {
-		rateString = rateString + "&#xf005;";
-	};
-	return rateString;
+  var rateString = "";
+  for (var i = 0; i < rate; i++) {
+    rateString = rateString + "&#xf005;";
+  };
+  return rateString;
 });
-
-////////// Helpers for in-place editing //////////
-
-// Returns an event map that handles the "escape" and "return" keys and
-// "blur" events on a text input (given by selector) and interprets them
-// as "ok" or "cancel".
-
-// var okCancelEvents = function (selector, callbacks) {
-//   var ok = callbacks.ok || function () {};
-//   var cancel = callbacks.cancel || function () {};
-
-//   var events = {};
-//   events['keyup '+selector+', keydown '+selector+', focusout '+selector] =
-//     function (evt) {
-//       if (evt.type === "keydown" && evt.which === 27) {
-//         // escape = cancel
-//         cancel.call(this, evt);
-
-//       } else if (evt.type === "keyup" && evt.which === 13 ||
-//                  evt.type === "focusout") {
-//         // blur/return/enter = ok/submit if non-empty
-//         var value = String(evt.target.value || "");
-//         if (value)
-//           ok.call(this, value, evt);
-//         else
-//           cancel.call(this, evt);
-//       }
-//     };
-
-//   return events;
-// };
-
-var activateInput = function (input) {
-  input.focus();
-  input.select();
-};
--- client/igralci.js
+++ client/igralci.js
@@ -36,30 +36,6 @@ Template.igralci.izbran_igralec = function () {
   return Igralci.findOne(Session.get("izbran_igralec"));
 };
 
-var okCancelEvents = function (selector, callbacks) {
-  var ok = callbacks.ok || function () {};
-  var cancel = callbacks.cancel || function () {};
-
-  var events = {};
-  events['keyup '+selector+', keydown '+selector+', focusout '+selector] =
-    function (evt) {
-      if (evt.type === "keydown" && evt.which === 27) {
-        // escape = cancel
-        cancel.call(this, evt);
-
-      } else if (evt.type === "keyup" && evt.which === 13 ||
-                 evt.type === "focusout") {
-        // blur/return/enter = ok/submit if non-empty
-        var value = String(evt.target.value || "");
-        if (value)
-          ok.call(this, value, evt);
-        else
-          cancel.call(this, evt);
-      }
-    };
-
-  return events;
-};
 
 Template.igralec.events(okCancelEvents('#nov-igralec', {
   ok: function(text, event){
@@ -67,12 +43,3 @@ Template.igralec.events(okCancelEvents('#nov-igralec', {
     event.target.value = "";
   }
 }));
-
-// Template.igralec.events({
-//   'click #nov-igralec': function(event){
-//   event.target.value = 'teta';
-// },
-// 'click #tt': function(){
-//   $('#nov-igralec').text("tt");
-// }
-// });
